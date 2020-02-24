1682f24c0dc4e5ac5ebb38e819e0063d03cd4a26
Nicolas Lara <nicolaslara@Nicolass-MBP.attlocal.net>
2020-02-23 19:14:43 -0800

    intro unit completed plus control flow dir created

---
+++ javaScript/intro/script.js
@@ -0,0 +1,10 @@
+// var firstName=prompt("What is your first name?")
+// var lastName=prompt("What is your last name?")
+// var age=prompt("How old are you??")
+
+// console.log("Your full name is "+firstName+" "+lastName+".")
+// console.log("You are "+age+" years old.")
+
+var age=prompt("how old are you?")
+days=age*365.25
+alert(age+" is approximately "+days+" days.")
