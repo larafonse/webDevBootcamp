58b97760e24d13e344c3f9010d16e36ee00e1bf3
Nicolas Lara <nicolaslara@Nicolass-MBP.attlocal.net>
2020-04-06 21:46:13 -0700

    successfully added db and show route

--- yelpCamp/app.js
+++ yelpCamp/app.js
@@ -1,42 +1,84 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost:27017/yelp_camp', {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(bodyParser.urlencoded({extended:true}));
+ 
 // allows us to render ejs file wwithout having to put .ejs at the end
 app.set("view engine","ejs");
 
-var camps = [
-    {name:"Oak Park", img:"https://cdn.greenfieldnews.com/wp-content/uploads/sites/13/2019/10/1b4f02f05b4fb79d2ad6a0826e37884f.jpg"},
-    {name:"Pinnacles", img:"https://media-cdn.tripadvisor.com/media/photo-m/1280/17/62/b2/c1/pinnacles-national-park.jpg"},
-    {name:"San Lorenzo Park", img:"https://www.kingcity.com/wp-content/uploads/2015/04/san-lorenzo-park-2.png"},
-    {name:"Oak Park", img:"https://cdn.greenfieldnews.com/wp-content/uploads/sites/13/2019/10/1b4f02f05b4fb79d2ad6a0826e37884f.jpg"},
-    {name:"Pinnacles", img:"https://media-cdn.tripadvisor.com/media/photo-m/1280/17/62/b2/c1/pinnacles-national-park.jpg"},
-    {name:"San Lorenzo Park", img:"https://www.kingcity.com/wp-content/uploads/2015/04/san-lorenzo-park-2.png"
-];
+// SCHEMA TEMPLATE
+var campSchema = new mongoose.Schema({
+    name:String,
+    img:String,
+    description:String
+});
+
+var camp = mongoose.model("Camp",campSchema);
+
+// camp.create({
+//     name:"Patriot Park", 
+//     img:"https://media-cdn.tripadvisor.com/media/photo-m/1280/17/62/b2/c1/pinnacles-national-park.jpg",
+//     description:"This is a very patriotic park. Also contains a skate park"
+// },function(err,camp){
+//         if(err){
+//             console.log(err);
+//             }else{
+//                 console.log(`${camp.name} SUCCESSFULLY ADDED TO DATABASE`);
+//             }
+//     });
+
 // home route
 app.get("/",function(req,res){
     res.render("home");
 });
-// campground route
-app.get("/campgrounds", function(req,res){
-    
-    res.render("campgrounds",{camps:camps});
-});
 
-app.get("/campgrounds/new", function(req,res){
-    res.render("new.ejs");
+// INDEX - show all campgrounds
+app.get("/campgrounds", function(req,res){
+    // Get campgrounds from database
+    camp.find({},function(err,camps){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("campgrounds",{camps:camps});
+        }
+    });  
 });
+// CREATE - create new campground
 app.post("/campgrounds", function(req, res){
     // get data from form and add to campgorunds array
     var name = req.body.name;
     var image = req.body.image;
-    console.log(image)
-    var newCampground = {name:name,img:image}
-    camps.push(newCampground)
+    var desc = req.body.description;
+    var newCampground = {name:name,img:image,description:desc}
+    
+    camp.create(newCampground,function(err,camp){
+        if(err){
+            console.log(err);
+            }else{
+                console.log(`${camp.name} SUCCESSFULLY ADDED TO DATABASE`);
+            }
+    });
+    
     // redirect back to campgroundsroute
     res.redirect("/campgrounds");
 
+});
+// NEW - Schow form to create new campground
+app.get("/campgrounds/new", function(req,res){
+    res.render("new.ejs");
+});
+// 
+app.get("/campgrounds/:id", function(req,res){
+    camp.findById(req.params.id, function(err, foundCamp){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("show", {camp :foundCamp});
+        }
+    });
 });
 
 app.listen(3000,function() { 
